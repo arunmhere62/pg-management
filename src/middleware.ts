c62506dde971926a8b52a436de670e6e4c97362a
arunmhere62 <arunmhere62@gmail.com>
2025-04-15 23:47:01 +0530

    changes in auth and bugs

--- src/middleware.ts
+++ src/middleware.ts
@@ -13,3 +13,5 @@ export async function middleware(req: NextRequest) {
 export const config = {
   matcher: ['/((?!api|_next/static|_next/image|favicon.ico|login).*)']
 };
+
+export const runtime = 'nodejs';
